5de400d2f05768847068141813581f3525842013
ergeakova <ergeak@gmail.com>
2022-07-05 01:16:16 +0300

    created Interaction

--- CryptoPricesVIPERExample/Presenter.swift
+++ CryptoPricesVIPERExample/Presenter.swift
@@ -6,3 +6,33 @@
 //
 
 import Foundation
+
+enum NetworkError: Error {
+    case networkFaild
+    case ParsingFaild
+}
+
+protocol AnyPresenter{
+    var router: AnyRouter? {get set}
+    var interactor: AnyInteractor? {get set}
+    var view: AnyView? {get set}
+    
+    func interactorDidDownloadCrypto(result: Result<[Crypto], Error>)
+}
+
+class CryptoPresenter : AnyPresenter {
+    var router: AnyRouter?
+    var interactor: AnyInteractor?
+    var view: AnyView?
+    
+    func interactorDidDownloadCrypto(result: Result<[Crypto], Error>) {
+        switch result {
+        case .success(let cryptos):
+            print("Update!")
+        case .failure(let error):
+            print("Error!")
+        }
+    }
+    
+    
+}
--- CryptoPricesVIPERExample/View.swift
+++ CryptoPricesVIPERExample/View.swift
@@ -6,3 +6,12 @@
 //
 
 import Foundation
+import UIKit
+
+protocol AnyView{
+    
+}
+
+class CryptoView: UIViewController, AnyView{
+    
+}
